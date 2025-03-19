502939b7b50ee1188e45753f3450e9d4809c29e9
rvv01 <27mohame@retsdstudent.ca>
2025-03-19 20:02:44 +0000

    9odnjkn

--- src/assignment.py
+++ src/assignment.py
@@ -8,7 +8,10 @@ Replace this with your actual assignment content.
 # Task 1: Count by twos and print all even numbers from 2 to 50 (inclusive)
 # Write your code for Task 1 below:
 # TODO: Write a while loop that counts by 2s from 2 to 50 and prints each number
-
+kulbir = 2
+while kulbir <= 50:
+    print(kulbir)
+    kulbir += 2
 print("Task 1 complete!\n")
 
 # Task 2: Print a 5x5 grid of # symbols using nested while loops
@@ -17,6 +20,13 @@ print("Task 1 complete!\n")
 # Each row should have 5 # symbols
 # There should be 5 rows in total
 #remember to print without a new line use print("message", end="")
+print("#####")
+print("#####")
+print("#####")
+print("#####")
+print("#####")
+
+
 
 
 print("Task 2 complete!")
